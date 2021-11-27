5b66325744bc596645bd3fb59f0c8ed7524e2b6a
SzyRo151582 <szomyn3@gmail.com>
2021-11-27 09:59:41 +0100

    labolatorium nr 2 rozwiazania zadan

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,10 +10,29 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+        int x = 4;
+        for(int i = 100;i >= 0;i--)
+        {
+            if (i % x == 0) System.out.println("Liczba "+i+" jest podzielna przez ostatnia cyfre mojego indeksu - "+x);
+        }
+
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
+
+        Scanner scan = new Scanner(System.in);
+        int tab1[] = new int[5];
+
+        System.out.print("Podaj element nowej tablicy: ");
+
+        for (int i = 0; i < tab1.length; i++) {
+            tab1[i] = scan.nextInt();
+        }
+
+        for (int zmienna : tab1){
+            System.out.print(" "+zmienna+" ");
+        }
 
 
     }
