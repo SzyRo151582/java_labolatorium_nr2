e8a4b53af6ecf75f3cec0e0c6ebca7b2613cf8cd
SzyRo151582 <szomyn3@gmail.com>
2021-12-06 17:49:14 +0100

    labolatorium nr 2 rozwiazania zadan

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,27 @@ public class Main {
         */
 
 
+        for (int x = 0; x <= 20; x++) {
+            if (x == 15)
+                break;
+            System.out.print(x + " ");
+        }
+
+        System.out.println("\n");
+
+        for (int y = 0; y <= 15; y++) {
+            if (y % 5 == 0)
+                continue;
+            System.out.print(y + " ");
+        }
+
+        System.out.println("\n");
+
+        for (int z = 0; z <= 80; z++) {
+            if (z == 55)
+                return;
+            System.out.print(z + " ");
+        }
+
     }
 }
