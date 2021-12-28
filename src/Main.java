f014e3227c1f855807fac449a10ca44fcae27d44
SzyRo151582 <szomyn3@gmail.com>
2021-12-28 19:40:46 +0100

    labolatorium nr 2 rozwiazania zadan

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,80 @@ import java.util.*;
 
 public class Main {
 
+    public static void noweZwierze(List<String> lista1)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj nazwe nowego zwierzecia: ");
+            String zwierze = scan.nextLine();
+            lista1.add(zwierze);
+        }
+    }
+    
+    public static void listaZwierzat(List<String> lista1)
+    {
+        for (String zmienna : lista1)
+        {
+            System.out.print(zmienna + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public static void zmianaListy(List<String> lista1)
+    {
+        lista1.remove(7);
+        lista1.remove(6);
+
+        for (int i = 0; i <= 2; i++)
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj nazwe nowego zwierzecia: ");
+            String zwierze = scan.nextLine();
+            lista1.add(zwierze);
+        }
+
+        for (String zmienna : lista1)
+        {
+            System.out.print(zmienna + " ");
+        }
+
+        System.out.println(lista1.size());
+    }
+
+    public static void sortowanieListy(List<String> lista1)
+    {
+        Collections.sort(lista1, Collections.reverseOrder());
+    }
+
+    public static void noweLiczby(Set<Integer> lista2)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj nowa liczbe: ");
+            int liczba = scan.nextInt();
+            lista2.add(liczba);
+        }
+    }
+
+    /*public static void sortowanieListy(Set<Integer> lista2)
+    {
+        Collections.sort(lista2);
+    }*/
+
+    public static void listaLiczb(Set<Integer> lista2)
+    {
+        int i = 1;
+
+        for (int zmienna : lista2)
+        {
+            System.out.print(i + ":" + zmienna + " ");
+            i++;
+        }
+        System.out.println("\n");
+    }
+
     public static void main(String[] args) {
 
         /* ćw_6 Listy */
@@ -19,12 +93,24 @@ public class Main {
                 elementów listy
           */
 
+        List<String> zwierzeta = new ArrayList<>();
+
+        noweZwierze(zwierzeta);
+        listaZwierzat(zwierzeta);
+        zmianaListy(zwierzeta);
+        sortowanieListy(zwierzeta);
+        listaZwierzat(zwierzeta);
+
 
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
 
-    }
+        Set<Integer> liczby = new TreeSet<>();
+        noweLiczby(liczby);
+        //sortowanieListy(liczby);
+        listaLiczb(liczby);
 
+    }
 }
